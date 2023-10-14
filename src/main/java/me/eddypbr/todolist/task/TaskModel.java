f1eea4bd4f9564c1ff2d5b1be3b72413aa9a0eb2
EddyPBR <edvaldojunior1310@gmail.com>
2023-10-14 08:39:53 -0300

    :bug: fix TaskModel set title validation

--- src/main/java/me/eddypbr/todolist/task/TaskModel.java
+++ src/main/java/me/eddypbr/todolist/task/TaskModel.java
@@ -36,7 +36,7 @@ public class TaskModel {
   private LocalDateTime CreatedAt;
 
   public void setTitle(String title) throws Exception {
-    if (title.length() > 0) {
+    if (title.length() > 50) {
       throw new Exception("Title must be have less then 50 characters");
     }
 
